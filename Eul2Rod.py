a6943ec741319dc4b7027f60d9097b23e0c0fb20
Sai Viswanadh <sai-viswanadha-sastry.upadhyayula@student.tu-freiberg.de>
2020-05-13 22:54:04 +0200

    Update files

--- Eul2Rod.py
+++ Eul2Rod.py
@@ -1,6 +1,7 @@
 from Eul2Axan import Eul2Axa
 from Axan2Rod import Axa2Rod
 def Eul2Rod(x,y,z):
-    N = Eul2Axa(x,y,z)
-    return(Axa2Rod(N))
+    n = Eul2Axa(x,y,z)[0]
+    w = Eul2Axa(x,y,z)[1]
+    return(Axa2Rod(n,w))
 Eul2Rod(360,0,360)
