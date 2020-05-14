51ea302f129f45dc44420c8749c2852d76054fa5
Sai Viswanadh <sai-viswanadha-sastry.upadhyayula@student.tu-freiberg.de>
2020-05-14 09:59:09 +0200

    Update Axan2Ho.py file

--- Axan2Ho.py
+++ Axan2Ho.py
@@ -1,12 +1,12 @@
 import math
 import numpy as np
-def Axan2Ho(n,w):
-    W = math.radians(w)
-    f = ((0.75)*(W-math.sin(W)))**(1/3)
-    h = np.zeros(len(n))
-    for i in range(0,len(h)):
-        h[i] = f*n[i]
+def Axan2Ho(N):
+    h = np.zeros((len(N),len(N[0])-1))
+    for j in range(0,len(N)):
+        W = math.radians(N[j][3])
+        f = ((0.75)*(W-math.sin(W)))**(1/3)
+        for i in range(0,len(h)):
+            h[j][i] = f*N[j][i]
     return h
-n = [0,-0,-1]
-w = 90
-print(Axan2Ho(n,w))
+N = [[0,0,1,0],[0,0,1,90],[0,0,1,180]]
+print(Axan2Ho(N))
