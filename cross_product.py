93213c4b21a5398994906345172d59b9863045a1
Sai Viswanadh <sai-viswanadha-sastry.upadhyayula@student.tu-freiberg.de>
2020-05-14 11:10:11 +0200

    Update cross_product.py file

--- cross_product.py
+++ cross_product.py
@@ -1,10 +1,13 @@
+import numpy as np
 from permutation_tensor import permutation
+#This function returns a cross product of two vectors
 def cross_product(a,b):
-    m = []
-    n = [0]*len(a)
+    n = np.zeros(len(a))
     for k in range(0,len(n)):
         for i in range(0,len(a)):
             for j in range(0,len(b)):
-                n[k] = n[k]+(permutation(i,j,k)*a[i]*b[j])
-        m.append(n[k])
-    return m
+                n[k] = n[k]+(permutation(i,j,k)*a[i]*b[j]) #Calculating cross product of two vectors and assigning it to a vector 
+    return n
+a = [1,2,3]
+b = [6,8,9]
+print(cross_product(a,b))
