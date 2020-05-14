4485b317cefe21794cda7147af5b488f6d8cc323
Sai Viswanadh <sai-viswanadha-sastry.upadhyayula@student.tu-freiberg.de>
2020-05-14 13:53:02 +0200

    Update files

--- Qu2Axan.py
+++ Qu2Axan.py
@@ -2,21 +2,22 @@ import math
 import numpy as np
 from permutation_tensor import sgn
 from modulus import modulus
+#This function transforms quarternions to axis angle pair representation
 def Qu2Axan(q):
-    w = round(2*math.acos(q[0]),5)
-    if w == 0:
-        n = [0,0,1]
-        return n,w
-    else:
-        if q[0] == 0:
-            n = [q[1],q[2],q[3]]
-            w = math.degrees(math.pi)
-            return n,w 
+    n = np.zeros((len(q),4)) #Creating array with zeros in it
+    for L in range(0,len(q)):
+        n[L][3] = round(math.degrees(round(2*math.acos(q[L][0]),5)))#Calculating angle 
+        if n[L][3] == 0: #special case if angle of rotation is 0
+            n[L] = [0,0,1,0]
         else:
-            s = sgn(q[0])/(modulus(q[1:4]))
-            n = np.zeros(3)
-            for i in range(0,len(n)):
-                n[i] = s*q[i+1]
-            return n,round(math.degrees(w))
-q = [0.7071068 ,0.0000000, 0.0000000, 0.7071068] 
+            if q[L][0] == 0: #if scalar part of quarternion is 0
+                n[L][0:3] = [q[L][1],q[L][2],q[L][3]] #calculating axis 
+                n[L][3] = math.degrees(math.pi) #equating angle to pi
+            else:
+                s = sgn(q[L][0])/(modulus(q[L][1:4]))
+                for i in range(0,3):
+                    n[L][i] = s*q[L][i+1]
+    return n
+# Testing the function for few values
+q = [[1.0000000, 0.0000000, 0.0000000, 0.0000000],[0.7071068 ,0.0000000, 0.0000000, 0.7071068],[0.0000000 ,0.0000000, 0.0000000, 1.0000000]]
 print(Qu2Axan(q))
--- Qu2Ho.py
+++ Qu2Ho.py
@@ -1,18 +1,22 @@
 import math
 import numpy as np
 from modulus import modulus
+#This function transforms quarternions to homochoric representation
 def Qu2Ho(q):
-    w = 2*math.acos(q[0])
-    h = np.zeros(3)
-    if w == 0:
-        return h 
-    else:
-        s = 1/modulus((q[1:4]))
-        n = np.zeros(3)
-        f = 3*(w - math.sin(w))/4
-        for i in range(0,len(n)):
-            n[i] = s*q[i+1]
-            h[i]  = n[i]*(f**(1/3))
-        return h
-q = [0,0,0,1]
+    h = np.zeros((len(q),3))
+    w = np.zeros(len(q))
+    s = np.zeros(len(q))
+    f = np.zeros(len(q))
+    n = np.zeros((len(q),3))
+    for j in range(0,len(q)):
+        w[j] = 2*math.acos(q[j][0]) #first converting scalar part of quarternion to angle of rotation  
+        if w[j] != 0: #If angle of rotation is not equal to zero the following program works
+            s[j] = 1/modulus((q[j][1:4])) 
+            f[j] = 3*(w[j] - math.sin(w[j]))/4
+            for i in range(0,len(n)):
+                n[j][i] = s[j]*(q[j][i+1]) #converting vector part of quarternions to axis vector and then transforming 
+                h[j][i]  = n[j][i]*(f[j]**(1/3))#homochoric representation
+    return h
+# Testing the function for few values
+q = [[1.0000000, 0.0000000, 0.0000000, 0.0000000],[0.7071068 ,0.0000000, 0.0000000, 0.7071068],[0.0000000 ,0.0000000, 0.0000000, 1.0000000]]
 print(Qu2Ho(q))
